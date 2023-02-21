0a544604f34e67e528f5036e6804b1e6b7cf7035
Barbara Pagano <kotokokairapb@gmail.com>
2023-02-20 21:55:06 -0400

    validacion para retornar a pagina principal mientras no haya nada en el carro

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ThemeContext from '../context/ThemeContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Cart() {
+  const { state } = React.useContext(ThemeContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(state.cart.length === 0){
+      navigate('/');
+    }
+  }, [state.cart])
   return (
     <div>
       hola
