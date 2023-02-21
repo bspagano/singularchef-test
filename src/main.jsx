2db25576050211da2712e5580ca3324cf38e606f
Barbara Pagano <kotokokairapb@gmail.com>
2023-02-20 21:21:57 -0400

    :eliminando lineas en desuso

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
-// import './sass/index.scss';
-
-
-
-import ThemeContext from "./context/ThemeContext";
-import useInitialState from "./hooks/useInitialState";
 import App from "./App";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
